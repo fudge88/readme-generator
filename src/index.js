215e53785183a10e3cc8fda631b6edfd20132063
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-11-16 23:25:21 +0000

    added async await to prompt function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// import inquirer
+const inquirer = require("inquirer");
+
+//questions
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "what is your project title?",
+  },
+];
+
+const start = async () => {
+  const titleAnswer = await inquirer.prompt(questions);
+  console.log(titleAnswer);
+};
+
+start();
